2cc505408b2220aa73e91d811bd0234e95819e0f
witehound <netvid00@gmail.com>
2023-01-24 12:56:42 +0100

    completed setup for pages in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, List, Hotel } from "./Pages";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<List />} />
+          <Route path="/hotel/:id" element={<Hotel />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home/Home";
+import List from "./List/List";
+import Hotel from "./Hotel/Hotel";
+
+export { Home, List, Hotel };
